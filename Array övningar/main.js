68c0156e006cb2130314562a92623dddb75e3193
David Heidari <davidheidari2@gmail.com>
2023-11-20 14:14:57 +0100

    some chenges to my array

---
+++ Array övningar/main.js
@@ -0,0 +1,18 @@
+// användaq splice() metoden i en array
+
+//let month = ['januari', 'februari', 'mars'];
+//let days = ['måndag']
+
+
+let month = ['januari', 'februari', 'mars'];
+let days = month.splice(2, 2, 'mars', 'hello');
+
+console.log(days)
+console.log(month)
+
+
+
+
+// splice()
+// slice()
+// join()
