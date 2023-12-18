194159fcc0b14dfe8ee9a5155183047a36c234f4
David Heidari <davidheidari2@gmail.com>
2023-12-18 14:28:00 +0100

    added some js file

---
+++ variabelOmfång/main.js
@@ -0,0 +1,25 @@
+/* Scope:
+- Globala 
+- function
+- Block : måsvingar 
+*/
+let globalScope = 'globalScope';
+
+function greet() {
+    let functionScope = 'functionScope';
+    if (1 === 1) {
+        let blockScope = 'blockScope';
+        console.log(blockScope)
+        console.log(functionScope)
+    }
+
+    for(let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+
+    //console.log(blockScope)
+}
+greet();
+console.log(a + b)
+
+
